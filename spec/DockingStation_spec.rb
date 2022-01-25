a68cc56dee94018c15a9cc97ac22884519ed0163
Charlotte Buchan <cebuc@talktalk.net>
2022-01-25 17:03:05 +0000

    wrote first test

---
+++ spec/DockingStation_spec.rb
@@ -0,0 +1,9 @@
+describe DockingStation do
+    describe "#release" do
+    it "should release bikes" do 
+        docking_station = DockingStation.new
+        expect(docking_station).eq "Release"
+   
+    end
+  end
+end
