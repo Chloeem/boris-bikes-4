fdb049577b74a500e921722ba0afa37d9e117cee
Chloe <chloeemstokes@gmail.com>
2022-01-28 16:06:00 +0000

    Raise exceptions 2

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,18 +2,18 @@ require_relative './bike.rb'
 class DockingStation
   attr_reader :bike
 
-  # def initialize 
-  #   @bikes = []
-  # end
+  def initialize 
+    @bikes = []
+  end
 
   def release_bike
-    fail "No bikes available at this time" unless @bike
-    @bike
+    fail "No bikes available at this time" unless @bikes
+    @bikes
   end
 
   def dock_bike(bike)
-    @bike = bike
-    # @bikes < bike 
+    # @bike = bike
+    @bikes << bike 
   end
 
   # def docked?
